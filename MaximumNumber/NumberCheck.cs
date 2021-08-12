babdcf3d84218025d10ed9d1490d64d6723aef82
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-12 12:50:56 -0600

    feat:Maximum number of 3 integers

---
+++ MaximumNumber/NumberCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumNumber
+{
+    public class NumberCheck
+    {
+        public static int MaximumNumberCheck(int firstnumber, int secondnumber, int thirdnumber)
+        {
+            if (firstnumber.CompareTo(secondnumber) > 0 && firstnumber.CompareTo(thirdnumber) > 0) 
+            {
+                return firstnumber;
+            }
+            if (secondnumber.CompareTo(firstnumber) > 0 && secondnumber.CompareTo(thirdnumber) > 0) 
+            {
+                return secondnumber;
+            }
+            if (thirdnumber.CompareTo(firstnumber) > 0 && thirdnumber.CompareTo(secondnumber) > 0)
+            {
+                return thirdnumber;
+            }
+            throw new Exception("firstnumber,secondnumber,thirdnumber are same");
+        }
+    }
+
+}
+
--- MaximumNumber/Program.cs
+++ MaximumNumber/Program.cs
@@ -7,6 +7,8 @@ namespace MaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum value programs using generic");
+            int output = NumberCheck.MaximumNumberCheck(22, 33, 44);
+            Console.WriteLine(output);
 
               
         }
